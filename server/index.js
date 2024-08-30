72299a5aae6598b359133dd85301e9f53eed8f2e
Yousef eslam <yousefeslam214@gmail.com>
2024-08-30 04:20:11 +0300

    update

--- server/index.js
+++ server/index.js
@@ -20,17 +20,24 @@ const cors = require('cors');
 
 
 // CORS configuration
-// app.use(cors());
+app.use(cors());
+// app.use(cors({ origin: '*' }));
 
-app.use(cors({
-    origin: '*',
-    credentials: true
-}));
+
+// app.use(cors({
+//     origin: '*', // Allows all origins. Adjust this as needed for security.
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true
+// }));
+
+
 app.use(express.json());
 
 
 // CORS configuration
 // const allowedOrigins = ['https://warehouse-management-system-01.netlify.app'];
+// const allowedOrigins = ['http://127.0.0.1:5500/'];
 // app.use(cors({
 //     origin: function (origin, callback) {
 //         // allow requests with no origin (like mobile apps or curl requests)
@@ -51,16 +58,16 @@ app.use(express.json());
 // // CORS configuration
 // const allowedOrigins = ['http://localhost:5173']; // Your React app's URL
 
-app.use(cors({
-    origin: function (origin, callback) {
-        // if (!origin) return callback(null, true);
-        // if (allowedOrigins.indexOf(origin) === -1) {
-        //     const msg = 'The CORS policy for this site does not allow access from the specified origin.';    
-        //     return callback(new Error(msg), false);
-        // }
-        return callback(null, true);
-    },
-}));
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         // if (!origin) return callback(null, true);
+//         // if (allowedOrigins.indexOf(origin) === -1) {
+//         //     const msg = 'The CORS policy for this site does not allow access from the specified origin.';    
+//         //     return callback(new Error(msg), false);
+//         // }
+//         return callback(null, true);
+//     },
+// }));
 
 
 app.get('/', (req, res) => {
